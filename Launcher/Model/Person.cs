76344c43953e7cfa52d20e704feff264bd351f7d
lsrovnal <Lukas.Srovnal.extern@buerse-stuttgart.de>
2024-07-25 21:50:44 +0200

    Add class model (incoming data)

---
+++ Launcher/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Launcher.Model;
+
+public class Person
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+    
+    public GenderType Gender { get; set; }
+    
+    public string Address { get; set; }  = string.Empty;
+    
+    public string JobPosition { get; set; } = string.Empty;
+    
+    public short Age { get; set; }
+}
+
+public enum GenderType
+{
+    Man,
+    Woman
+}
